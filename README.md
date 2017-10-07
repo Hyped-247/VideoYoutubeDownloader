454d06574d243b05f87f28fb7f2cd2b1b1ab7437
Mohammad Mahjoub <mmahjoub@westmont.edu>
2017-10-06 22:50:49 -0700

    Fixing spelling stuff

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# VideoYoubtubeDownloader
+# VideoYoutubeDownloader
 - This App is going to allow you to download Youtube Videos from your desktop. Gone are the days
 where you have to look up 100 website to just download a video.
--- main.py
+++ main.py
@@ -1,4 +1,48 @@
 from pytube import YouTube
+from tkinter import *
+
+class App(Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+
+        self.url = Entry()
+        self.url.pack()
+
+        self.path = Entry() # This creates a place to pass information
+        self.path.pack()
+
+        # here is the application variable
+        self.contents = StringVar()
+        # set it to some value
+        self.contents.set("Enter Video url:")
+
+
+        # tell the entry widget to watch this variable
+        self.url["textvariable"] = self.contents
+
+        # and here we get a callback when the user hits return.
+        # we will have the program print out the value of the
+        # application variable when the user hits return
+        self.url.bind('<Key-Return>', self.print_contents)
+
+    def print_contents(self, event):
+        print("hi. contents of entry is now ---->",
+              self.contents.get())
+
+# create the application
+myapp = App()
+
+#
+# here are method calls to the window manager class
+#
+myapp.master.title("VideoYoutubeDownloader")
+myapp.master.maxsize(1000, 400)
+
+# start the program
+myapp.mainloop()
+
+
 
 
 def get_video():
@@ -17,3 +61,5 @@ def get_video():
     vid.download(destination)
 
     print(yt.filename+"\nHas been successfully downloaded")
+
+
