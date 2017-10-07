39c78115b9056a4e2be00eccda1c4ae2f4fef655
Mohammad Mahjoub <mmahjoub@westmont.edu>
2017-10-06 22:12:27 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is the main file from the master class. 
+from pytube import YouTube
+
+yt = YouTube(str(input("Enter the video link: ")))
+videos = yt.get_videos()
+
+s = 1
+for v in videos:
+    print(str(s)+". "+str(v))
+    s += 1
+
+n = int(input("Enter the number of the video: "))
+vid = videos[n-1]
+
+destination = str(input("Enter the destination: "))
+vid.download(destination)
+
+print(yt.filename+"\nHas been successfully downloaded")
