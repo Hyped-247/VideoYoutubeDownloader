aedd2159c4aa72165daeb57ef406c3fd7a8a85b5
Mohammad Mahjoub <mmahjoub@westmont.edu>
2017-10-24 15:18:12 -0700

    Fixing the clear bug

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from pytube import YouTube
 from tkinter import *
-
+from pytube import *
 # ==================================================Settings=======================================================
 root = Tk()
 root.title("Video Youtube Downloader") # set up the title and size.
@@ -16,8 +15,7 @@ left = Frame(root,  width=600, height=400, bg='black')
 left.pack(side=LEFT)
 right = Frame(root, width=250, height=400)
 right.pack(side=RIGHT)
-# ==================================================Functions=======================================================
-
+# ==================================================Functions===================================================
 
 def clear_all():
     clear_url()
@@ -33,9 +31,11 @@ def clear_path():
     path_text.set("")
     print("This is running" + "clear_path"+ path.get())
 
+def download_video(url):
+    pass
 
 
-# ==================================================Buttons=======================================================
+# ==================================================Buttons======================================
 btn_clear_url = Button(right, text="Clear Url", font=('arial', 25, 'bold'), command=lambda: clear_url())\
     .grid(row=1, columns=1)
 btn_clear_path = Button(right, text="Clear Path", font=('arial', 25, 'bold'), command=lambda: clear_path())\
@@ -45,18 +45,25 @@ btn_clear_all = Button(right, text="Clear all", font=('arial', 25, 'bold'),
     .grid(row=5, columns=1)
 btn_down_video = Button(right,  text="Download Video", font=('arial', 22, 'bold'))\
     .grid(row=7, columns=1)
-# ==================================================Labels======================================================
-title = Label(top,  font=('arial', 20, 'bold'), text="YouTube Videos Downloader", bd=5, anchor='w')
-title.grid(row=0, columns=1)
-feedback = Label(bottom, font=('arial', 20, 'bold'), text="YouTube Videos Downloader", bd=5, anchor='w')
-feedback.grid(row=0, columns=1)
-# ==================================================Variables===================================================
+# ==================================================Labels========================================
+# This is the top
+top_lable = Label(top,  font=('arial', 20, 'bold'), text="YouTube Videos Downloader", bd=5, anchor='w')
+top_lable.grid(row=0, columns=1)
+
+bottom_lable = Label(bottom, font=('arial', 20, 'bold'), text="YouTube Videos Downloader", bd=5, anchor='w')
+bottom_lable.grid(row=0, columns=1)
+
+
+
+
+
+# ==================================================Variables======================================
 path_text = StringVar()
 url_text = StringVar()
-# ==================================================Entry=======================================================
+# ==================================================Entry=======================================
 url = Entry(left, font=('arial', 20, 'bold'), width=35, textvariable=url_text, bd=8, insertwidth=7,
             bg="powder blue")
-url.grid(row=3, columns=1)
+url.grid(row=0, columns=1)
 path = Entry(left, font=('arial', 20, 'bold'), width=35, textvariable=path_text, bd=8, insertwidth=7,
              bg="powder blue")
 path.grid(row=1,  sticky=W+N)
