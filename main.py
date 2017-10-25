13b5af8d3a41b6f6e0c1947acb166017b327f725
Mohammad Mahjoub <mmahjoub@westmont.edu>
2017-10-24 22:49:59 -0700

    Fixing the Radiobuttons

--- main.py
+++ main.py
@@ -24,30 +24,39 @@ def clear_all():
 
 
 def clear_url():
-    print(url_text)
     url_text.set("")
 
 
 def clear_path():
-    print (path_text)
     path_text.set("")
+
+
+def action():
+    print("Funcation call.... ")
+    if len(url.get()) > 0 and len(path.get()) > 0:
+        return ACTIVE
+    else:
+        return DISABLED
 
 
 def download_video(url):
     youtube_url = str(url)
     if youtube_url.startswith("https://www.youtube.com/"):
         yt = YouTube(youtube_url)
-        videos = yt.streams.filter(adaptive=True).all()
+        videos = yt.streams.all()
         if quality.get() == 0:
-            video = videos.first()
-            # video = videos.last()
-            video.download(path.get())
+            video = videos[0]
+        else:
+            video = videos[len(videos) - 1]
+        video.download(path.get())
     else:
         # tell the user that this is is no a url.
         print("Please enter a correct url... "+url)
-
-
-
+# ==================================================Variables===================================
+url_text = StringVar()
+path_text = StringVar()
+video_nummber = StringVar()
+quality = StringVar()
 # ==================================================Buttons======================================
 btn_clear_url = Button(right, text="Clear Url", font=('arial', 25, 'bold'), command=lambda: clear_url())\
     .grid(row=1, columns=1)
@@ -62,40 +71,31 @@ btn_down_video = Button(right,  text="Download Video", font=('arial', 22, 'bold'
 # This is the top lable
 top_lable = Label(top,  font=('arial', 20, 'bold'), text="YouTube Videos Downloader", bd=5, anchor='w')
 top_lable.grid(row=0, columns=1)
-
 # This is the bottom lable
 bottom_lable = Label(bottom, font=('arial', 20, 'bold'), text="YouTube Videos Downloader", bd=5, anchor='w')
 bottom_lable.grid(row=0, columns=1)
-
-# This is the left lable
-
 # This is for the url
 url_lable = Label(left, font=('arial', 20, 'bold'), text="Url: ",  anchor='w')
 url_lable.grid(row=1, columns=1)
-
 # This is for the path
 path_lable = Label(left, font=('arial', 20, 'bold'), text="Path: ",  anchor='w')
 path_lable.grid(row=2, columns=1)
 
-# ==================================================Variables===================================
-url_text = StringVar()
-path_text = StringVar()
-video_nummber = StringVar()
-quality = StringVar()
-# ==================================================Radiobutton===================================
-
-btn1_data = Radiobutton(left, text="Good Quality",  font=('bold', 20), variable=quality, value=0)
-btn1_data.grid(row=0,  column=1, sticky=W)
-
-btn2_data = Radiobutton(left, text="Bad Quality",  font=('bold', 20), variable=quality, value=1)
-btn2_data.grid(row=0,  column=1)
 # ==================================================Entry=======================================
-# The width should be 37
 url = Entry(left, font=('arial', 20, 'bold'), width=37, bd=8, insertwidth=7,
             bg="powder blue")
 url.grid(row=1,  column=1, sticky=E+S)
 path = Entry(left, font=('arial', 20, 'bold'), width=37, bd=8, insertwidth=7, bg="powder blue")
 path.grid(row=2, column=1, sticky=E+S)
+# ==================================================Radiobutton===================================
+btn1_data = Radiobutton(left, text="Good Quality",  font=('bold', 20), variable=quality,
+                        value=0, state=action())
+btn1_data.grid(row=0,  column=1, sticky=W)
+btn2_data = Radiobutton(left, text="Bad Quality",  font=('bold', 20), variable=quality,
+                        value=1, state=action())
+btn2_data.grid(row=0,  column=1)
 
 
 root.mainloop()
+ # /Users/mohammad/Desktop
+# https://www.youtube.com/watch?v=48LtMYGOrpY
